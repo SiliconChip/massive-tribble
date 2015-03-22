1970e6d20d0e0db93554c848442d217bab461af6
Kenzie Togami <ket1999@gmail.com>
2015-03-21 22:54:36 -0700

    Spooky secure data.

--- deployer.py
+++ deployer.py
@@ -3,11 +3,15 @@ import cgitb
 
 cgitb.enable()
 
+# Secure data we know about
+securedata = __import__('securevars')
 headers = {'Content-Type': 'text/plain'}
 output = ''
 
 
 def main():
+    global output, headers
+    output += securedata.SECURE_MESSAGE + '\n'
     print_headers()
     print_content()
 
